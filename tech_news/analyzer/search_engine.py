2e2ffcae5386a9c33d73f06dc396808058179f71
Mixchelle <michelle.biologia@hotmail.com>
2023-07-30 14:12:09 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,11 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    query = {"title": {"$regex": title, "$options": "i"}}
+    result = db.news.find(query)
+    return [(news["title"], news["url"]) for news in result]
 
 
 # Requisito 8
